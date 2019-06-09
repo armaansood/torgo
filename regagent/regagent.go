0db3d809d13567951d94272acfbe8599ab97e92a
Armaan Sood <armaansood@outlook.com>
2019-06-08 21:44:50 -0700

    test udp4

--- regagent/regagent.go
+++ regagent/regagent.go
@@ -52,7 +52,7 @@ func (a *Agent) StartAgent(regServerIP string, regServerPort string, debug bool)
 	address.WriteString(regServerIP)
 	address.WriteString(":")
 	address.WriteString(regServerPort)
-	conn, err := net.Dial("udp", address.String())
+	conn, err := net.Dial("udp4", address.String())
 	fmt.Println(conn.RemoteAddr())
 	if err != nil {
 		log.Fatal(err)
